679054f308e57f2106f22e7d8cca54bdd764facf
Mayank <bhatasanamayank06@gmail.com>
2025-08-02 15:55:59 +0530

    changes

--- script.js
+++ script.js
@@ -1,85 +1,96 @@
-class choice{
-    name;
-    botChoice;
-    userChoice;
-    didUserWins;
-    userScore = 0;
-    botScore = 0;
-    choices = {
-        1 : "rock",
-        2 : "paper",
-        3 : "scissors",
-    }
+const userScore = document.querySelector('#user-score');
+const computerScore = document.querySelector('#computer-score');
+const numOfTies = document.querySelector('#the-number-of-tie');
+const displayAnswer = document.querySelector('.answer');
+const selectedOption = document.querySelector('.option');
+const restartButton = document.querySelector('#restart');
 
-    getWinner = {
-        'win': ["rock" , "scissors"],
-        'win2' : ["paper" , "rock"],
-        'win3' : ["scissors" , "paper"],
-    }
+const history = {
 
-    greet(){
-        this.name = prompt("What is your name?");
-        console.log(`Welcome to the rock, paper and scissors games ` + this.name +"!!");
-        console.log(this.botChoice);
-    }
-
-    getUsersChoice(){
-        return this.choices[parseInt(prompt(`What is your choice ${this.name}?
-        1)Rock
-        2)Paper
-        3)scissors
-        `))];
-    }
-
-    hasArrayValue = function (obj, target) {
-        return Object.values(obj).some(arr =>
-            arr.length === target.length &&
-            arr.every((item, index) => item === target[index])
-        );
-    }
-
-    getComputerChoice = function (){
-        return this["choices"][Math.ceil(Math.random() * 3)];
-    }
-
-    announceWinner() {
-        if (this.userChoice === this.botChoice) {
-            console.log(`It's a tie!`);
-        }
-        else {
-            if (this.didUserWins) {
-                console.log("You won!");
-                console.log(this.userChoice, this.botChoice);
-                this.userScore++;
-            } else {
-                console.log("You lose!");
-                console.log(this.userChoice, this.botChoice);
-                this.botScore++;
-            }
-        }
-
-    }
-
-    /*
-    * return {int}
-    * */
-    whoWon = function(){
-
-        return this.hasArrayValue(this.getWinner, [this.userChoice, this.botChoice])
-    }
 }
 
-const user = new choice();
-user.greet();
-
-while(true){
-    user.botChoice = user.getComputerChoice()
-    user.userChoice = user.getUsersChoice();
-    user.didUserWins = user.whoWon();
-    user.announceWinner();
-
-    const ans = prompt("do you want to play (Y)es or (N)o?");
-    if (ans === 'N' || ans === 'n') break;
+// get the computer choice when the user click on the options
+const getComputerChoice = function (){
+  const choices = {
+    1 : "rock",
+    2 : "paper",
+    3 : "scissors",
+  }
+  return choices[Math.ceil(Math.random() * 3)];
 }
 
-console.log(`\n\n\nAnd the winner is: ${user.userScore > user.botScore ? user.name : "bot"} 🎉`);
+const displayClassRemove = function (){
+  displayAnswer.classList.remove('win');
+  displayAnswer.classList.remove('lose');
+  displayAnswer.classList.remove('tie');
+}
+
+const resetTheScore = function (){
+  userScore.textContent = 0;
+  numOfTies.textContent = 0;
+  computerScore.textContent = 0;
+}
+resetTheScore();
+
+restartButton.addEventListener('click', function () {
+  displayClassRemove();
+  displayAnswer.textContent = `Make your choice!`;
+  resetTheScore()
+  document.getElementById('user-choice-img').src = `img/question-mark.png`;
+  document.getElementById('computer-choice-png').src = `img/question-mark.png`;
+});
+
+
+selectedOption.addEventListener('click', function (e) {
+  // Get users choice
+  const userChoice = e.target.closest('.hoverImg') ?.dataset.choice;
+
+  // return if the user did not select the option
+  if(!userChoice) return;
+
+  //get the computer choice just as the use select the option
+  const computerChoice = getComputerChoice();
+
+  // remove the class before add the new class / color
+  displayClassRemove();
+
+  // conclude who won
+
+  // both have the same answer it's a tie
+  if(computerChoice === userChoice){
+    //make the num of ties a number
+    numOfTies.textContent = 1 + Number(numOfTies.textContent);
+
+    // change the color and the text
+    displayAnswer.classList.add('tie');
+    displayAnswer.textContent = `It's tie`;
+  }
+
+  // the user won
+  else if(
+    (userChoice === 'rock' && computerChoice === 'scissors') ||
+    (userChoice === 'paper' && computerChoice === 'rock') ||
+    (userChoice === 'scissors' && computerChoice === 'paper')
+  ){
+    userScore.textContent = 1 + Number(userScore.textContent)
+
+    // change the color and the text
+    displayAnswer.classList.add('win');
+    displayAnswer.textContent = `You win!`
+  }
+
+  // the computer won
+  else{
+    computerScore.textContent = 1 + Number(computerScore.textContent)
+
+    // change the color and the text
+    displayAnswer.classList.add('lose');
+    displayAnswer.textContent = `Computer win!`
+  }
+
+
+  // change the hand symbol
+  document.getElementById('user-choice-img').src = `img/${userChoice}.png`;
+  document.getElementById('computer-choice-png').src = `img/${computerChoice}.png`;
+  console.log(userChoice, computerChoice);
+})
